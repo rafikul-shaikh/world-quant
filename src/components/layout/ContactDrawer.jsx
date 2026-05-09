9c8b24ad307cd828bd81f217c787823ff7076df7
Rafikul shaikh <rafikulshaikh510@gmail.com>
2026-05-09 11:15:06 +0530

    contactDrawer file done

--- src/components/layout/ContactDrawer.jsx
+++ src/components/layout/ContactDrawer.jsx
@@ -1,8 +1,14 @@
 import { X } from "lucide-react";
 import Button from "../sections/ui/Button";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
 
 export default function ContactDrawer({ open, setOpen }) {
+  const items = ["FOUNDER", "INVESTOR", "PARTNER", "JOURNALIST", "OTHER"];
+  const [selectedItem, setSelectedItem] = useState(items[0]);
+  const [phone, setPhone] = useState("");
+
   useEffect(() => {
     if (open) {
       document.body.style.overflow = "hidden";
@@ -14,8 +20,6 @@ export default function ContactDrawer({ open, setOpen }) {
       document.body.style.overflow = "auto";
     };
   }, [open]);
-
-  const items = ["FOUNDER", "INVESTOR", "PARTNER", "JOURNALIST", "OTHER"];
 
   return (
     <>
@@ -29,8 +33,8 @@ export default function ContactDrawer({ open, setOpen }) {
 
       {/* Drawer */}
       <div
-        className={`fixed top-0 left-0 z-[100] h-screen overflow-y-auto hide-scrollbar w-full max-w-[700px] bg-[#dadada] text-black  px-12 py-10  rounded-r-[20px] 
-           shadow-2xl  transition-transform duration-500 ease-in-out ${open ? "translate-x-0" : "-translate-x-full"}`}
+        className={`fixed top-0 left-0 z-[100] h-screen overflow-y-auto hide-scrollbar w-full max-w-[700px] bg-[#dadada] text-black 
+           px-12 py-10  rounded-r-[20px] shadow-2xl  transition-transform duration-500 ease-in-out ${open ? "translate-x-0" : "-translate-x-full"}`}
       >
         <button
           onClick={() => setOpen(false)}
@@ -61,20 +65,37 @@ export default function ContactDrawer({ open, setOpen }) {
         {/* item section */}
         <div>
           <h2 className="text-[11px] font-azeret uppercase">
-            I'm a <span className="text-gray-500">[SELECT]</span>
+            I'm a <span className="text-gray-500">[SELECT ONE]</span>
           </h2>
-          <div className=" mt-4  flex flex-row gap-12">
+          <div className=" mt-4  flex flex-row gap-4 font-azeret text-[14px] text-gray-500">
             {items.map((item) => (
-              <button key={item}>{item}</button>
+              <button
+                key={item}
+                type="button"
+                onClick={() => setSelectedItem(item)}
+                className={`group flex items-center px-2 py-2 cursor-pointer transition-all duration-300 
+                  ${selectedItem === item ? "gap-2 bg-white/50" : "hover:bg-white/40 hover:text-black"}`}
+              >
+                {/* Square */}
+
+                {selectedItem === item && (
+                  <div className="w-2.5 h-2.5 rounded-[3px] bg-orange-400"></div>
+                )}
+                <span
+                  className={` transition-colors duration-300 ${selectedItem === item ? "text-orange-500" : ""}  `}
+                >
+                  {item}
+                </span>
+              </button>
             ))}
           </div>
         </div>
 
         {/* form section */}
-        <div className="pt-10 pb-20 ">
-          <form className=" text-[14px] flex flex-col gap-4">
+        <div className="pt-8 pb-10 ">
+          <form className="text-[14px] flex flex-col ">
             <p className="text-[11px] uppercase">About You</p>
-            <div className="mb-6 grid grid-cols-2 gap-4 ">
+            <div className="mb-8 grid grid-cols-2 gap-5">
               <div>
                 <label htmlFor="firstname"></label>
                 <input
@@ -83,7 +104,7 @@ export default function ContactDrawer({ open, setOpen }) {
                   name="firstname"
                   placeholder="First name"
                   required
-                  className="border-b border-black p-2 w-full focus:outline-none"
+                  className="border-b border-black/20 py-2 w-full focus:outline-none"
                 />
               </div>
 
@@ -95,7 +116,7 @@ export default function ContactDrawer({ open, setOpen }) {
                   name="lastname"
                   placeholder="Last name"
                   required
-                  className="border-b border-black p-2 w-full focus:outline-none"
+                  className="border-b border-black/20 py-2 w-full focus:outline-none"
                 />
               </div>
 
@@ -107,23 +128,31 @@ export default function ContactDrawer({ open, setOpen }) {
                   name="email"
                   placeholder="Email"
                   required
-                  className="border-b border-black p-2 w-full focus:outline-none"
+                  className="border-b border-black/20 py-2 w-full focus:outline-none"
                 />
               </div>
-              <div>
-                <label htmlFor="phone"></label>
-                <input
-                  type="tel"
-                  id="phone"
-                  name="phone"
-                  placeholder="Phone"
-                  required
-                  className="border-b border-black p-2 w-full focus:outline-none"
+
+              <div className="relative border-b border-black/20 ">
+                <PhoneInput
+                  country={"us"}
+                  value={phone}
+                  onChange={(phone) => setPhone(phone)}
+                  enableSearch={true}
+                  countryCodeEditable={false}
+                  disableCountryCode={false}
+                  placeholder=""
+                  containerClass="phone-container"
+                  inputClass="phone-input"
+                  buttonClass="phone-dropdown"
                 />
+
+                {phone === "" && (
+                  <span className="phone-placeholder">Phone</span>
+                )}
               </div>
             </div>
-            <p className="uppercase">Your business</p>
-            <div>
+            <p className="text-[11px] uppercase">Your business</p>
+            <div className="">
               <div>
                 <label htmlFor="yourbusiness"></label>
                 <input
@@ -132,7 +161,7 @@ export default function ContactDrawer({ open, setOpen }) {
                   name="yourbusiness"
                   placeholder="Business name"
                   required
-                  className="border-b border-black p-2 w-full focus:outline-none"
+                  className="border-b border-black/20 py-4 w-full focus:outline-none"
                 />
               </div>
 
@@ -144,25 +173,32 @@ export default function ContactDrawer({ open, setOpen }) {
                   name="typeofbusiness"
                   placeholder="Type of business"
                   required
-                  className="border-b border-black p-2 w-full focus:outline-none"
+                  className="border-b border-black/20 py-4 w-full focus:outline-none"
                 />
                 <textarea
                   id="message"
                   name="message"
                   placeholder="Message (Optional)"
-                  className="border-b border-black p-2 w-full bg-transparent resize-none focus:outline-none"
+                  className="border-b border-black/20 py-3 w-full bg-transparent resize-none focus:outline-none"
                 />
               </div>
             </div>
-            <div className="text-gray-500 uppercase flex flex-row gap-1">
-              <input type="checkbox" id="privacy" required />
+            <div className=" text-[11px] font-azeret text-gray-500 uppercase pt-6 flex flex-row gap-1">
+              <input
+                type="checkbox"
+                id="privacy"
+                required
+                className="w-4 h-4 bg-gray-400 cursor-pointer "
+              />
 
               <label htmlFor="privacy">
                 By checking this box I agree to the{" "}
-                <span className="text-black underline">privacy policy</span>
+                <span className="text-black underline hover:no-underline cursor-pointer">
+                  privacy policy
+                </span>
               </label>
             </div>
-            <div>
+            <div className="pt-10">
               <Button
                 padding="px-7 py-3"
                 textColor="text-black/80"
